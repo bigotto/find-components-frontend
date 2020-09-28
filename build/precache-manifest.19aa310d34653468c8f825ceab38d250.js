833561033f1e440805dd464912ed03fb3aa7c0f8
Bruno Bigotto <bbigotto@gmail.com>
2020-09-28 09:55:44 -0400

    Preparando para deploy no Heroku

---
+++ build/precache-manifest.19aa310d34653468c8f825ceab38d250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5be56ed2eeb3dae14fa16f8709406f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9bb968dc1027a70fb80",
+    "url": "/static/css/main.5adca3bb.chunk.css"
+  },
+  {
+    "revision": "5ed6ccbe1b87889031f4",
+    "url": "/static/js/2.c78ff059.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c78ff059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9bb968dc1027a70fb80",
+    "url": "/static/js/main.26940b78.chunk.js"
+  },
+  {
+    "revision": "7e38ccfd5ee7463bf27e",
+    "url": "/static/js/runtime-main.e89cd7ba.js"
+  },
+  {
+    "revision": "fc648670b98c4242aeed4684a886b0b8",
+    "url": "/static/media/logo.fc648670.png"
+  }
+]);
