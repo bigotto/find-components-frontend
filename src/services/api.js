c45c333f49e1796796f63d378dce13332ceccf63
Bruno Bigotto <bbigotto@gmail.com>
2020-09-28 10:44:51 -0400

    Ajuste do endereço da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.21.80:3333'
+    baseURL: 'https://find-components-backend.herokuapp.com/'
 })
 
 export default api;
